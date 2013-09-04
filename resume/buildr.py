64e9218c0104b46124b5c2d7ef00502af06395f6
Ian Dexter R Marquez <iandexter+tech@gmail.com>
2013-09-04 17:11:46 +0800

    Transform Markdown-style links to TeX-style href.

--- resume/buildr.py
+++ resume/buildr.py
@@ -9,8 +9,44 @@ import json
 import codecs
 import jinja2
 import os
+import re
 
-__version__ = '0.1'
+__version__ = '0.2'
+
+def objwalk(obj, path = (), memo = None):
+    """From http://code.activestate.com/recipes/577982-recursively-walk-python-objects/"""
+    from collections import Mapping, Set, Sequence
+
+    string_types = (str, unicode) if str is bytes else (str, bytes)
+    iteritems = lambda mapping: getattr(mapping, 'iteritems', mapping.items)()
+
+    if memo is None:
+        memo  = set()
+    iterator = None
+    if isinstance(obj, Mapping):
+        iterator = iteritems
+    elif isinstance(obj, (Sequence, Set)) and not isinstance(obj, string_types):
+        iterator = enumerate
+    if iterator:
+        if id(obj) not in memo:
+            memo.add(id(obj))
+            for path_component, value in iterator(obj):
+                for result in objwalk(value, path + (path_component,), memo):
+                    yield result
+            memo.remove(id(obj))
+    else:
+        yield path, obj
+
+def transform(obj, patt, repl):
+    """ From http://stackoverflow.com/questions/11501090/iterate-over-nested-lists-and-dictionaries"""
+    regex = re.compile(patt)
+    for p, v in objwalk(obj):
+        if isinstance(v, basestring):
+            if regex.findall(v):
+                new_obj = obj
+                for s in p[:-1]:
+                    new_obj = new_obj[s]
+                new_obj[p[-1]] = regex.sub(repl, v)
 
 def load_json(json_file = None):
     return json.load(codecs.open(json_file, 'r', 'utf-8-sig'))
@@ -36,12 +72,16 @@ def render_resume():
 
 if __name__ == '__main__':
     JSON_FILE = 'resume-iandexter.json'
-    EXT = 'tex'
-    TEMPLATE = "resume.%s.j2" % EXT
-    OUTPUT = 'resume-iandexter.%s' % EXT
+    ftype = 'tex'
+    TEMPLATE = "resume.%s.j2" % ftype
+    OUTPUT = 'resume-iandexter.%s' % ftype
 
     resume_data = load_json(JSON_FILE)['resume']
     resume_data[u'keywords'] = get_keywords(resume_data)
+
+    link_marker = '\\[([^\\]]+)\\]\\(([^\\)]+)\\)'
+    link_sub = r'\\href{\2}{\1}'
+    transform(resume_data, link_marker, link_sub)
 
     resume_template = render_resume().get_template(TEMPLATE)
     with open(OUTPUT, 'w') as resume:
