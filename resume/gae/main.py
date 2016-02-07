9c45155007edc22709326d5c0277a37cb511d8f4
Ian Dexter D Marquez <iandexter@users.noreply.github.com>
2016-02-08 00:15:01 +0800

    Use webapp2 framework

--- resume/gae/main.py
+++ resume/gae/main.py
@@ -1,14 +1,14 @@
-from google.appengine.ext import webapp
+import webapp2 as webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class AllHandler(webapp.RequestHandler):
     def get(self):
         self.redirect("http://www.iandexter.net/resume", True)
 
-application = webapp.WSGIApplication([('/.*', AllHandler)])
+app = webapp.WSGIApplication([('/.*', AllHandler)])
 
 def main():
-    run_wsgi_app(application)
+    run_wsgi_app(app)
 
 if __name__ == '__main__':
     main()
